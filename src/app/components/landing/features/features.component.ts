1cc30a5bcdb889b5a766788af90ce415a34c2545
Oscar Zambrano <oscarperez.everis@latam.com>
2019-03-30 12:16:23 -0300

    click features flip

--- src/app/components/landing/features/features.component.ts
+++ src/app/components/landing/features/features.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
   @Input() userType: any;
+  card = 0;
   constructor() { }
 
   ngOnInit() {
     console.log(this.userType);
   }
 
+  selectCard(idCard) {
+    if (idCard !== this.card) {
+      this.card = idCard;
+    } else {
+      this.card = null;
+    }
+  }
 }
